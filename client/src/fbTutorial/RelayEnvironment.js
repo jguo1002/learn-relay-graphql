2f5e4a5e5204ac1b6ab0c392d43dbd2a4a42190d
jguo1002 <guojiajing1002@gmail.com>
2022-07-27 08:03:31 -0700

    add code from FB tutorial

---
+++ client/src/fbTutorial/RelayEnvironment.js
@@ -0,0 +1,16 @@
+// https://relay.dev/docs/getting-started/step-by-step-guide/
+
+import { Environment, Network, RecordSource, Store } from "relay-runtime";
+import fetchGraphQL from "./fetchGraphQL";
+
+async function fetchRelay(params, variables) {
+  console.log(
+    `fetching query ${params.name} with ${JSON.stringify(variables)}`
+  );
+  return fetchGraphQL(params.text, variables);
+}
+
+export default new Environment({
+  network: Network.create(fetchRelay),
+  store: new Store(new RecordSource()),
+});
